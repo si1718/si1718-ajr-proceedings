1d943cc63cac39e25d7985f25f86d92088042e3e
Andres Jimenez <andresjimenezrios@gmail.com>
2017-11-26 12:37:29 +0000

    Angular frontend: edit

--- public/js/create-ctrl.js
+++ public/js/create-ctrl.js
@@ -5,18 +5,13 @@ var API_HTTP = "/api/v1/proceedings"
 angular.module("ProceedingManagerApp")
     .controller("CreateCtrl", ["$scope", "$http", "$location", function($scope, $http, $location) {
         function initialice(){
-            $scope.new_proceeding = {
-                "title": "",
-                "year": "",
-                "editor": "",
-                "coeditors": [],
-                "publisher": "",
-                "city": "",
-                "country": ""
-            };
+            $scope.new_proceeding = {};
         }
         
         $scope.createProceeding = function() {
+            if($scope.new_proceeding.coeditors.length > 0) {
+                $scope.new_proceeding.coeditors = $scope.new_proceeding.coeditors.split("\n");
+            }
             $http
                 .post(API_HTTP, $scope.new_proceeding)
                 .then((response) => {
---
+++ public/js/edit-ctrl.js
@@ -0,0 +1,38 @@
+/*global angular*/
+
+var API_HTTP = "/api/v1/proceedings"
+
+angular.module("ProceedingManagerApp")
+    .controller("EditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
+        
+        function initialice(){
+            $http
+                .get(API_HTTP + "/" + $routeParams.idProceeding)
+                .then((response) => {
+                    $scope.updated_proceeding = response.data;
+                    
+                    if($scope.new_proceeding.coeditors.length > 0) {
+                        $scope.updated_proceeding.coeditors = $scope.updated_proceeding.coeditors.join("\n");
+                    }
+                    delete $scope.updated_proceeding._id;
+                }, (err) => {
+                    alert(err.data);
+                });
+        }
+        
+        $scope.updateProceeding = function() {
+            if($scope.new_proceeding.coeditors.length > 0) {
+                $scope.updated_proceeding.coeditors = $scope.updated_proceeding.coeditors.split("\n");
+            }
+            
+            $http
+                .put(API_HTTP + "/" + $scope.updated_proceeding.idProceeding, $scope.updated_proceeding)
+                .then((response) => {
+                    $location.path("/");
+                }, (err) => {
+                    alert(err.data);
+                });
+        };
+        
+        initialice();
+    }]);

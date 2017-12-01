708cc4a95e13b823c028c24f4044435fffb0d75f
Andres Jimenez <andresjimenezrios@gmail.com>
2017-12-01 10:46:46 +0000

    Add cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var db = require("./db");
 var bodyParser = require("body-parser");
 var helmet = require("helmet");
 var path = require("path");
+var cors = require('cors');
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json()); //use default json enconding/decoding
 app.use(helmet()); //improve security
+app.use(cors());
 
 var BASE_PATH = "/api/v1";
 var ProceedingController = require("./proceeding/ProceedingController");
